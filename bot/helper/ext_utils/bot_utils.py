777bcd961282f6df7a735f296447e759e8fdaa80
asasasww1 <98530492+asasasww1@users.noreply.github.com>
2022-01-31 19:45:22 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -125,7 +125,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"<b>ชื่อ:</b> <code>{download.name()}</code>"
+            msg += f"<b>ชื่อไฟล์:</b> <code>{download.name()}</code>"
             msg += f"\n<b>สถานะ:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
